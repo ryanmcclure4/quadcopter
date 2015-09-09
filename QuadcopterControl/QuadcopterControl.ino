74648a4c89a156692d36a35ec3463ff07c9aa7fb
Ryan McClure <ryanmcclure4@gmail.com>
2015-09-08 18:31:33 -0700

    Organized / Rearranged code

--- QuadcopterControl/QuadcopterControl.ino
+++ QuadcopterControl/QuadcopterControl.ino
@@ -14,22 +14,61 @@
 
 #define MAX_THROTTLE 2000
 #define MIN_THROTTLE 1000
-#define MAX_PID 100
-#define MIN_PID -100
+#define MAX_PID      100
+#define MIN_PID     -100
 
 // ESC Variables -------------------------------------------------------- |
-Servo m1, m2, m3, m4;
+Servo m1;
+Servo m2;
+Servo m3;
+Servo m4;
 
-// IMU Variables -------------------------------------------------------- |
+// IMU / MPU Variables -------------------------------------------------- |
 MPU6050 mpu;
 
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+bool     dmpReady = false;  // set true if DMP init was successful
+uint8_t  mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t  devStatus;      // return status after each device operation (0 = success, !0 = error)
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
+uint8_t  fifoBuffer[64]; // FIFO storage buffer
 
+// Motion / Orientation variables
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+// PID ----------------------------------------------------------------- |
+float setYaw      = 0;
+float setPitch    = 0;
+float setRoll     = 0;
+float inputYaw    = 0;
+float inputPitch  = 0;
+float inputRoll   = 0;
+float pidOutYaw   = 0;
+float pidOutPitch = 0;
+float pidOutRoll  = 0;
+
+float yawOffset   = 0;
+float pitchOffset = 0;
+float rollOffset  = 0;
+
+// tuning parameters
+float Kp = 5;
+float Ki = 5;
+float Kd = 1;
+
+PID yawPID  (&inputYaw,   &pidOutYaw,   &setYaw,   Kp, Ki, Kd, DIRECT);
+PID pitchPID(&inputPitch, &pidOutPitch, &setPitch, Kp, Ki, Kd, DIRECT);
+PID rollPID (&inputRoll,  &pidOutRoll,  &setRoll,  Kp, Ki, Kd, DIRECT);
+
+// Throttle / Parsing Variables ---------------------------------------- |
 float throttleIn = 1000;
 float throttle = 1000;
 float m1Throttle;
@@ -37,48 +76,10 @@ float m2Throttle;
 float m3Throttle;
 float m4Throttle;
 
-int indexCommaA;
-int indexCommaB;
-
 bool readThrottle = false;
 bool readPos = false;
 
-// variables to calculate pid values
-float setPitch = 0;
-float setRoll = 0;
-float setYaw = 0;
-float inputPitch = 0;
-float inputRoll = 0;
-float inputYaw = 0;
-float pidOutPitch = 0;
-float pidOutRoll = 0;
-float pidOutYaw = 0;
-
-// MPU control/status vars
-
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-    mpuInterrupt = true;
-}
-
-// tuning parameters
-float Kp = 5, Ki = 5, Kd = 1;
-
-// PID objects
-PID yawPID(&inputYaw, &pidOutYaw, &setYaw, Kp, Ki, Kd, DIRECT);
-PID pitchPID(&inputPitch, &pidOutPitch, &setPitch, Kp, Ki, Kd, DIRECT);
-PID rollPID(&inputRoll, &pidOutRoll, &setRoll, Kp, Ki, Kd, DIRECT);
-
 String message = "";
-String yawString;
-String pitchString;
-String rollString;
 
 // ----------------------------------------------------------------------- |
 // Setup ----------------------------------------------------------------- |
@@ -147,11 +148,9 @@ void setup()
 // Loop ------------------------------------------------------------------ |
 void loop()
 {
-
-  // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize) {
-      
-    }
+    // Receive MPU data -------------------------------------------------- |
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) { }
 
     // reset interrupt flag and get INT_STATUS byte
     mpuInterrupt = false;
@@ -179,97 +178,96 @@ void loop()
         // (this lets us immediately read more without waiting for an interrupt)
         fifoCount -= packetSize;
 
-        // display Euler angles in degrees
+        // Delegate Yaw / Pitch / Roll calculation to DMP
         mpu.dmpGetQuaternion(&q, fifoBuffer);
         mpu.dmpGetGravity(&gravity, &q);
         mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-        //Serial.print("ypr\t");
-        //Serial.print(ypr[0] * 180/M_PI);
-        //Serial.print("\t");
-        //Serial.print(ypr[1] * 180/M_PI);
-        //Serial.print("\t");
-        //Serial.println(ypr[2] * 180/M_PI);
     }
 
-  //Read from bluetooth and write to usb serial
-  if(Serial.available())
-  {
-    char charIn = (char)Serial.read();
-    // Reading a throttle value
-    if (charIn == 't') {
-      Serial.println("READING THROTTLE VALUE");
-      readThrottle = true;
-      readPos = false;
-    } else if (charIn == 'p') {
-      Serial.println("READING POSITION VALUE");
-      readThrottle = false;
-      readPos = true;
-    } else {
-      Serial.println("Reading in character : " + (String)charIn);
-      if (charIn != 'n') {
-        message += charIn;
-        Serial.println("inchar : " + message);
-
-      // Reading in throttle values
-      } else if (readThrottle == true) {
-        throttleIn = atof(message.c_str());
-        throttleIn = map(throttleIn, 0, MIN_THROTTLE, 1040, MAX_THROTTLE);
-        if (throttleIn > 1030 && throttleIn <= MAX_THROTTLE)
-            throttle = throttleIn;
-        Serial.println("THROTTLE : " + message);
-        message = "";
-
-      // Reading in comma-delimited position values
-      } else if (readPos) {
-        indexCommaA = message.indexOf(',');
-        indexCommaB = message.indexOf(',', indexCommaA + 1);
-
-        yawString = message.substring(0, indexCommaA);
-        pitchString = message.substring(indexCommaA + 1, indexCommaB);
-        rollString = message.substring(indexCommaB + 1);
-
-        inputYaw = atof(yawString.c_str());
-        inputPitch = atof(pitchString.c_str());
-        inputRoll = atof(rollString.c_str());
-
-        Serial.println("YAW : " + yawString);
-        Serial.println("PITCH : " + pitchString);
-        Serial.println("ROLL : " + rollString);
+    // Receive user control data via bluetooth --------------------------- |
+    //Read from bluetooth and write to usb serial
+    if(Serial.available())
+    {
+        char charIn = (char)Serial.read();
         
-        message = "";
-      }
+        // Throttle value detected
+        if (charIn == 't') {
+          Serial.println("READING THROTTLE VALUE");
+          readThrottle = true;
+          readPos = false;
+
+        // Position value detected
+        } else if (charIn == 'p') {
+          Serial.println("READING POSITION VALUE");
+          readThrottle = false;
+          readPos = true;
+
+        // Read in data
+        } else {
+            if (charIn != 'n') {
+                message += charIn;
+                Serial.println("inchar : " + message);
+      
+            // Reading in throttle values
+            } else if (readThrottle == true) {
+                throttleIn = atof(message.c_str());
+                throttleIn = map(throttleIn, 0, MIN_THROTTLE, 1040, MAX_THROTTLE);
+                if (throttleIn > 1030 && throttleIn <= MAX_THROTTLE)
+                    throttle = throttleIn;
+                Serial.println("THROTTLE : " + message);
+                message = "";
+      
+            // Reading in position values
+            } else if (readPos) {
+                parseYawPitchRoll(message, inputYaw, inputPitch, inputRoll);
+                message = "";
+            }
+        }
     }
-  }
 
-  // Set current orientation and position
-  inputYaw = ypr[0] * 180/M_PI;
-  inputPitch = ypr[2] * 180/M_PI;
-  inputRoll = ypr[2] * 180/M_PI;
-
-  yawPID.Compute();
-  pitchPID.Compute();
-  rollPID.Compute();
-
-  m1Throttle = throttle + pidOutRoll + pidOutPitch + pidOutYaw;
-  m2Throttle = throttle - pidOutRoll + pidOutPitch - pidOutYaw;
-  m3Throttle = throttle + pidOutRoll - pidOutPitch - pidOutYaw;
-  m4Throttle = throttle - pidOutRoll - pidOutPitch + pidOutYaw;
-
-  Serial.print("OutYaw : ");
-  Serial.print((float)inputYaw,3);
-  Serial.print("\t");
-  Serial.print("OutPitch : ");
-  Serial.print((float)inputPitch,3);
-  Serial.print("\t");
-  Serial.print("RollPID : ");
-  Serial.print((float)inputRoll,3);
-  Serial.print("\t");
-  Serial.print((float)m1Throttle,3);
-  Serial.print("\t");
-  Serial.print((float)m2Throttle,3);
-  Serial.print("\t");
-  Serial.print((float)m3Throttle,3);
-  Serial.print("\t");
-  Serial.println((float)m4Throttle,3);
+    // Set current orientation and position
+    inputYaw   = ypr[0] * 180/M_PI + yawOffset;
+    inputPitch = ypr[1] * 180/M_PI + pitchOffset;
+    inputRoll  = ypr[2] * 180/M_PI + rollOffset;
+  
+    yawPID.Compute();
+    pitchPID.Compute();
+    rollPID.Compute();
+  
+    m1Throttle = throttle + pidOutRoll + pidOutPitch + pidOutYaw;
+    m2Throttle = throttle - pidOutRoll + pidOutPitch - pidOutYaw;
+    m3Throttle = throttle + pidOutRoll - pidOutPitch - pidOutYaw;
+    m4Throttle = throttle - pidOutRoll - pidOutPitch + pidOutYaw;
+  
+    Serial.print("OutYaw : ");
+    Serial.print((float)inputYaw,3);
+    Serial.print("\t");
+    Serial.print("OutPitch : ");
+    Serial.print((float)inputPitch,3);
+    Serial.print("\t");
+    Serial.print("RollPID : ");
+    Serial.print((float)inputRoll,3);
+    Serial.print("\t");
+    Serial.print((float)m1Throttle,3);
+    Serial.print("\t");
+    Serial.print((float)m2Throttle,3);
+    Serial.print("\t");
+    Serial.print((float)m3Throttle,3);
+    Serial.print("\t");
+    Serial.println((float)m4Throttle,3);
   
 }
+
+void parseYawPitchRoll (String data, float &yaw, float &pitch, float &roll) {
+    int indexCommaA = message.indexOf(',');
+    int indexCommaB = message.indexOf(',', indexCommaA + 1);
+
+    String yawString   = message.substring(0, indexCommaA);
+    String pitchString = message.substring(indexCommaA + 1, indexCommaB);
+    String rollString  = message.substring(indexCommaB + 1);
+
+    yaw   = atof(yawString.c_str());
+    pitch = atof(pitchString.c_str());
+    roll  = atof(rollString.c_str());
+}
+
